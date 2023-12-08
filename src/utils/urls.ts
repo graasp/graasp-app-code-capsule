11dd4a0bd1ef9dac84887421ac103180811efb13
spaenleh <spaenleh@gmail.com>
2023-12-08 17:24:08 +0100

    fix: eslint issues

--- src/utils/urls.ts
+++
@@ -1,8 +0,0 @@
-import qs from 'qs';
-
-export const getItemIdFromQueryString = (): string | undefined => {
-  const res = qs.parse(window.location.search, { ignoreQueryPrefix: true }) as {
-    itemId?: string;
-  };
-  return res?.itemId;
-};
